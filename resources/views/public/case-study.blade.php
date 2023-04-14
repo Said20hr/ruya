43ae09f65b8fd9d867ec6898ba05ee3123da4069
SAID HADJ RAMDANE <saidhr20@gmail.com>
2023-04-14 18:05:37 +0200

    update case study

--- resources/views/public/case-study.blade.php
+++ resources/views/public/case-study.blade.php
@@ -1,11 +1,10 @@
 <x-layout.errors title="{{$project->name }}">
     <div class="w-full min-h-screen xl:px-10 xl:py-4 p-2 container">
-        <div class="mb-10">
+        <div class="mb-6 text-center">
             <h2 class="mb-8 2xl:text-4xl xl:text-2xl text-xl dark:text-white">{{$project->name }}</h2>
-            <p>{{$project->excerpto }}</p>
+            <p class="dark:text-gray-200">{{$project->excerpto }}</p>
         </div>
-
-        <div class="mx-auto p-6">
+        <div class="mx-auto p-4">
         @if($project->additional_images)
             <div class="uk-position-relative rounded-xl" uk-slideshow="animation: fade">
                 <ul class="uk-slideshow-items rounded-xl">
@@ -31,7 +30,7 @@
                 </p>
             </div>
     </div>
-    <div class="flex w-fit justify-center my-12">
+    <div class="flex max-w-7xljustify-center py-12">
         <a href="{{ route('home',config('app.locale') ) }}" class="btn btn-primary z-50 relative">
             {{__('Return to homepage')}}
         </a>
