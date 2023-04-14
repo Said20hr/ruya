7c4902d3de90f0b4121376619c8a503df5c276c9
SAID HADJ RAMDANE <saidhr20@gmail.com>
2023-04-14 18:08:49 +0200

    update case study

--- resources/views/public/case-study.blade.php
+++ resources/views/public/case-study.blade.php
@@ -1,8 +1,8 @@
 <x-layout.errors title="{{$project->name }}">
     <div class="w-full min-h-screen xl:px-10 xl:py-4 p-2 container">
         <div class="mb-6 text-center">
-            <h2 class="mb-8 2xl:text-4xl xl:text-2xl text-xl dark:text-white">{{$project->name }}</h2>
-            <p class="dark:text-gray-200">{{$project->excerpto }}</p>
+            <h2 class="mb-8 2xl:text-5xl xl:text-4xl text-xl dark:text-white">{{$project->name }}</h2>
+            <p class="dark:text-gray-200 text-lg">{{$project->excerpto }}</p>
         </div>
         <div class="mx-auto p-4">
         @if($project->additional_images)
@@ -25,14 +25,15 @@
         @endif
         </div>
         <div class="container max-w-7xl">
-                <p class="">
-                    {!! $project->description  !!}
-                </p>
+            <p class="dark:text-gray-200">
+                {!! $project->description  !!}
+            </p>
+            <div class="flex justify-center py-12">
+                <a href="{{ route('home',config('app.locale') ) }}" class="btn btn-primary z-50 relative">
+                    {{__('Return to homepage')}}
+                </a>
             </div>
+        </div>
     </div>
-    <div class="flex max-w-7xljustify-center py-12">
-        <a href="{{ route('home',config('app.locale') ) }}" class="btn btn-primary z-50 relative">
-            {{__('Return to homepage')}}
-        </a>
-    </div>
+
 </x-layout.errors>
