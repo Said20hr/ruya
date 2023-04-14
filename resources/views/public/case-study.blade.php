d39e6231b751f081eb75899bf8e40eafb41838ca
SAID HADJ RAMDANE <saidhr20@gmail.com>
2023-04-14 18:18:19 +0200

    update case study

--- resources/views/public/case-study.blade.php
+++ resources/views/public/case-study.blade.php
@@ -1,5 +1,5 @@
 <x-layout.errors title="{{$project->name }}">
-    <div class="w-full min-h-screen xl:px-10 xl:py-4 p-2 container">
+    <div class="w-full min-h-screen xl:px-20 xl:py-4 p-2 container">
         <div class="mb-6 text-center">
             <h2 class="mb-8 2xl:text-5xl xl:text-4xl text-xl dark:text-white">{{$project->name }}</h2>
             <p class="dark:text-gray-200 text-lg">{{$project->excerpto }}</p>
@@ -25,12 +25,12 @@
         @endif
         </div>
         <div class="container max-w-7xl">
-            <p class="dark:text-gray-200">
+            <p class="dark:text-gray-200!">
                 {!! $project->description  !!}
             </p>
             <div class="flex justify-center py-12">
-                <a href="{{ route('home',config('app.locale') ) }}" class="btn btn-primary z-50 relative">
-                    {{__('Return to homepage')}}
+                <a href="{{ URL::previous() }}}" class="btn btn-primary z-50 relative">
+                    {{__('Return back')}}
                 </a>
             </div>
         </div>
